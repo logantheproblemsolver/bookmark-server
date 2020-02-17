e5da1cba4d28b3db984f95754fdc9cb4940a197f
Logan Ellis <mrloganellis@gmail.com>
2020-02-17 16:36:04 -0500

    finished bookmarks server

--- src/app.js
+++ src/app.js
@@ -1,38 +1,28 @@
 require('dotenv').config()
 const express = require('express')
-const morgan = require ('morgan')
+const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const {NODE_ENV} = require('./config')
+const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandler = require('./error-handler')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
+app.use(cors())
+app.use(helmet())
+app.use(validateBearerToken)
 
+app.use(bookmarksRouter)
 
-    app.use(morgan(morganOption))
-    app.use(helmet())
-    app.use(cors())
+app.get('/', (req, res) => {
+  res.send('Hello, world!')
+})
 
+app.use(errorHandler)
 
-
-    app.get('/', (req, res) => {
-        res.send("Hello, world!")
-    })
-
-    app.use(function errorHandler(error, req, res, next) {
-        let response 
-        if (NODE_ENV === 'production') {
-            response = {error: {message: 'server error'}}
-        } else {
-            console.error(error)
-            response = {message: error.message, error}
-        }
-        res.status(500).json(response)
-    })
-
-
-
-    module.exports = app
+module.exports = app
